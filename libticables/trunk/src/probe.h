4404175b622e674073060858fcfef468e12e6c7e
Julien Blache <jb@jblache.org>
2002-01-12 18:11:07 +0000

    Missing probe.h and pause.h
    git-svn-id: http://svn.tilp.info/repos/tilp@202

---
+++ libticables/trunk/src/probe.h
@@ -0,0 +1,30 @@
+/*  libticables - link cable library, a part of the TiLP project
+ *  Copyright (C) 1999-2002  Romain Lievin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __PROBE_H__
+#define __PROBE_H__
+
+#include <stdio.h>
+#include "export.h"
+
+int TICALL ticable_detect_os(char **os_type);
+int TICALL ticable_detect_port(PortInfo *pi);
+int TICALL ticable_detect_cable(PortInfo *pi);
+int TICALL ticable_detect_all(char **os, PortInfo *pi);
+
+#endif
