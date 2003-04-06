a5056a9c0adb209c82784b6ca5d0d4dde9963837
cvs2svn conversion <cvs2svn@tilp.info>
2003-04-06 13:59:55 +0000

    16 copies to tags/branches
    git-svn-id: http://svn.tilp.info/repos/tilp@201

---
+++ libticables/tags/libticables-3_2_4/src/logging.h
@@ -0,0 +1,45 @@
+/*  libticables - link cable library, a part of the TiLP project
+ *  Copyright (C) 1999-2002  Romain Lievin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <stdio.h>
+
+int start_logging();
+int log_data(int data);
+int stop_logging();
+
+int start_void();
+int log_void();
+int stop_void();
+
+/* uncomment it for enabling logging facilities */
+//#define LOGGING
+
+#ifdef LOGGING
+# define START_LOGGING(); start_logging();
+# define LOG_DATA(d);     log_data(d);
+# define STOP_LOGGING();  stop_logging();
+#else
+# define START_LOGGING();
+# define LOG_DATA(d);
+# define STOP_LOGGING();
+#endif
+
+#endif
