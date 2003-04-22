3e53255c9e6970d5fb0ed409da09ecc219641cf8
Julien Blache <jb@jblache.org>
2003-04-22 08:38:26 +0000

    Tagging libticalcs 4.4.8 final for Mac OS X.
    git-svn-id: http://svn.tilp.info/repos/tilp@259

---
+++ libticalcs/tags/4.4.8-osx/osx/config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/*
+ * $Id$
+ */
+
+#define VERSION "4.4.8"
+
+#define HAVE_TILP_MACROS_H
+#define HAVE_TILP_TYPEDEFS_H
+#define HAVE_TILP_TICABLES_H
+#define HAVE_TILP_TIFILES_H
+
+#endif /* !__CONFIG_H__ */
