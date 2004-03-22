56f4ebca8855612747355788b4052fe5a18773fe
Romain Liévin <roms@lievin.net>
2004-03-22 18:45:39 +0000

    Enabled versionning and file header updated.
    git-svn-id: http://svn.tilp.info/repos/tilp@369

--- libticalcs/trunk/src/probe.c
+++ libticalcs/trunk/src/probe.c
@@ -1,5 +1,8 @@
-/*  libticalcs - calculator library, a part of the TiLP project
- *  Copyright (C) 1999-2003  Romain Lievin
+/* Hey EMACS -*- linux-c -*- */
+/* $Id$ */
+
+/*  libticalcs - Ti Calculator library, a part of the TiLP project
+ *  Copyright (C) 1999-2004  Romain Lievin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- libticalcs/trunk/tests/test_ticalcs.c
+++ libticalcs/trunk/tests/test_ticalcs.c
@@ -1,5 +1,7 @@
-/*  libticalcs - calculator library, a part of the TiLP project
- *  Copyright (C) 1999-2002  Romain Lievin
+/* $Id$ */
+
+/*  libticalcs - Ti Calculator library, a part of the TiLP project
+ *  Copyright (C) 1999-2004  Romain Lievin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
