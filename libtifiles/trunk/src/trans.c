cc8c13553817c63ce42d0c1be10998ec5f50cf3b
Romain Liévin <roms@lievin.net>
2004-09-18 18:23:03 +0000

    Bug fix.
    git-svn-id: http://svn.tilp.info/repos/tilp@787

--- libtifiles/trunk/src/trans.c
+++ libtifiles/trunk/src/trans.c
@@ -911,7 +911,8 @@ TIEXPORT char* TICALL tifiles_transcode_to_utf8(char* dst, const char *src)
     {
     case CALC_TI73:
     case CALC_TI83:
-    case CALC_TI83P: f = transcode_from_ti83_charset_to_utf8; 
+    case CALC_TI83P: 
+	case CALC_TI84P: f = transcode_from_ti83_charset_to_utf8; 
       break;
     case CALC_TI82:  f = transcode_from_ti82_charset_to_utf8; 
       break;
@@ -919,6 +920,7 @@ TIEXPORT char* TICALL tifiles_transcode_to_utf8(char* dst, const char *src)
     case CALC_TI86:  f = transcode_from_ti85_charset_to_utf8; 
       break;
     case CALC_TI89:
+	case CALC_TI89T:
     case CALC_TI92:
     case CALC_TI92P:
     case CALC_V200:  f = transcode_from_ti9x_charset_to_utf8; 
