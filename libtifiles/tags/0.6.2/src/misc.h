2e9cba3dfb576df4995190103fa63ed60d68cf2d
Romain Liévin <roms@lievin.net>
2005-02-28 10:34:48 +0000

    ticalc.org release.
    git-svn-id: http://svn.tilp.info/repos/tilp@869

---
+++ libtifiles/tags/0.6.2/src/misc.h
@@ -0,0 +1,45 @@
+/* Hey EMACS -*- linux-c -*- */
+/* $Id$ */
+
+/*  libtifiles - Ti File Format library, a part of the TiLP project
+ *  Copyright (C) 1999-2004  Romain Lievin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __TIFILES_MISC__
+#define __TIFILES_MISC__
+
+int fread_n_chars(FILE * f, int n, char *s);
+int fwrite_n_chars(FILE * f, int n, const char *s);
+
+int fread_8_chars(FILE * f, char *s);
+int fwrite_8_chars(FILE * f, const char *s);
+
+int fskip(FILE * f, int n);
+
+int fread_byte(FILE * f, uint8_t * data);
+int fread_word(FILE * f, uint16_t * data);
+int fread_long(FILE * f, uint32_t * data);
+
+int fwrite_byte(FILE * f, uint8_t data);
+int fwrite_word(FILE * f, uint16_t data);
+int fwrite_long(FILE * f, uint32_t data);
+
+int hexdump(uint8_t * ptr, int len);
+
+int is_regfile(const char *filename);
+
+#endif
---
+++ libtifiles/tags/0.6.2/src/types92.h
@@ -0,0 +1,60 @@
+/* Hey EMACS -*- linux-c -*- */
+/* $Id$ */
+
+/*  libtifiles - Ti File Format library, a part of the TiLP project
+ *  Copyright (C) 1999-2004  Romain Lievin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __TIFILES_DEFS92__
+#define __TIFILES_DEFS92__
+
+#include "stdints.h"
+
+#define TI92_MAXTYPES 48
+
+#define TI92_EXPR 0x00
+#define TI92_LIST 0x04
+#define TI92_MAT  0x06
+#define TI92_DATA 0x0A
+#define TI92_TEXT 0x0B
+#define TI92_STR  0x0C
+#define TI92_GDB  0x0D
+#define TI92_FIG  0x0E
+#define TI92_PIC  0x10
+#define TI92_PRGM 0x12
+#define TI92_FUNC 0x13
+#define TI92_MAC  0x14
+#define TI92_RDIR 0x19
+#define TI92_BKUP 0x1D
+#define TI92_DIR  0x1F
+
+#define TI92_VNONE 0
+#define TI92_VLOCK 1
+
+// libtifiles: for internal use only, not exported !
+
+extern const char *TI92_CONST[TI92_MAXTYPES + 1][4];
+
+const char *ti92_byte2type(uint8_t data);
+uint8_t ti92_type2byte(const char *s);
+const char *ti92_byte2fext(uint8_t data);
+uint8_t ti92_fext2byte(const char *s);
+
+const char *ti92_byte2desc(uint8_t data);
+const char *ti92_byte2icon(uint8_t data);
+
+#endif
