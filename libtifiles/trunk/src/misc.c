80bd98997cfc9d53ffd6a89d4a66faac95109deb
Julien Blache <jb@jblache.org>
2005-05-13 19:39:59 +0000

    Endianness fixes.
    git-svn-id: http://svn.tilp.info/repos/tilp@1055

--- libtifiles/trunk/src/misc.c
+++ libtifiles/trunk/src/misc.c
@@ -28,6 +28,12 @@
 #include <string.h>
 #include "stdints.h"
 #include <sys/stat.h>
+#ifndef __WIN32__
+# include <endian.h>
+# if __BYTE_ORDER == __BIG_ENDIAN
+#  define WORDS_BIGENDIAN 1
+# endif /* __BYTE_ORDER == __BIG_ENDIAN */
+#endif /* !__WIN32__ */
 
 #include "export.h"
 #include "file_int.h"
