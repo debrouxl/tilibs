d9f5bc4155fdfd66ec524ba7ca0ba0264f476928
Romain Liévin <roms@lievin.net>
2005-07-21 17:03:31 +0000

    Merged PPhd's patch about i18n.
    git-svn-id: http://svn.tilp.info/repos/tilp@1415

--- libticables/trunk/src/intrface.c
+++ libticables/trunk/src/intrface.c
@@ -19,6 +19,13 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#if HAVE_LOCALE_H
+#include <locale.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
