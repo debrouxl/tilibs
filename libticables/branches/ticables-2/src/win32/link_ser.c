93b5b613b50f2a8ebad98bd486c7328f820769a4
Romain Liévin <roms@lievin.net>
2005-07-26 15:40:38 +0000

    Removed dbg printf.
    git-svn-id: http://svn.tilp.info/repos/tilp@1458

--- libticables/branches/ticables-2/src/win32/link_ser.c
+++ libticables/branches/ticables-2/src/win32/link_ser.c
@@ -200,8 +200,6 @@ static int ser_get(CableHandle *h, uint8_t *data, uint32_t len)
 	for(j = 0; j < len; j++)
 	{
 		uint8_t v, byte = 0;
-
-		printf(".");
   	
 		for (bit = 0; bit < 8; bit++) 
 		{
