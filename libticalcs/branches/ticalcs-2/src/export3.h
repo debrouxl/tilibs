8f2abb63c663bac49ea0b306d5c87a0991a02af2
Romain Liévin <roms@lievin.net>
2005-09-07 08:57:54 +0000

    Fixed non-exported symbol because exportX.h didn't include config.h (fvisibility).
    git-svn-id: http://svn.tilp.info/repos/tilp@1615

--- libticalcs/branches/ticalcs-2/src/export3.h
+++ libticalcs/branches/ticalcs-2/src/export3.h
@@ -19,8 +19,12 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#ifndef __TICABLES_EXPORT__
-#define __TICABLES_EXPORT__
+#ifndef __TICALCS_EXPORT__
+#define __TICALCS_EXPORT__
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
