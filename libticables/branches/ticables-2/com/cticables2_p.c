88e7ea55c5c29d693b1c94c1e2d669cb545646d6
Romain Liévin <roms@lievin.net>
2006-01-19 21:20:42 +0000

    Set $Id property and license header.
    git-svn-id: http://svn.tilp.info/repos/tilp@1708

--- libticables/branches/ticables-2/com/cticables2_p.c
+++ libticables/branches/ticables-2/com/cticables2_p.c
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Thu Jan 19 21:41:44 2006
+/* at Thu Jan 19 22:19:42 2006
  */
 /* Compiler settings for C:\sources\roms\ticables2\com\cticables2.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
