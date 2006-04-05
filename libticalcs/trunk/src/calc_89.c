1eabea0b9747b0e876498f405886095f2d6b7cba
Romain Liévin <roms@lievin.net>
2006-04-05 12:18:02 +0000

    Fixed get_version.
    git-svn-id: http://svn.tilp.info/repos/tilp@2131

--- libticalcs/trunk/src/calc_89.c
+++ libticalcs/trunk/src/calc_89.c
@@ -918,7 +918,13 @@ static int		get_version	(CalcHandle* handle, CalcInfos* infos)
 	snprintf(infos->os_version, 4, "%1i.%02i", buf[0], buf[1]);
 	snprintf(infos->boot_version, 4, "%1i.%02i", buf[2], buf[3]);
 	infos->battery = buf[4] == 1 ? 0 : 1;
-	infos->hw_version = buf[5];
+	switch(buf[13])
+	{
+	case 1:
+	case 3: infos->hw_version = buf[5] + 1;
+	case 8: infos->hw_version = buf[5];
+	case 9: infos->hw_version = buf[5] + 1;
+	}
 	switch(buf[13])
 	{
 	case 1: infos->model = CALC_TI92P; break;
