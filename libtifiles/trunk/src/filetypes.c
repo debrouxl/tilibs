45cf17516be7d4d6f83b67e409a173c93695083b
Romain Liévin <roms@lievin.net>
2006-12-08 16:25:41 +0000

    An TIB file is an OS, too. Fixed.
    git-svn-id: http://svn.tilp.info/repos/tilp@3103

--- libtifiles/trunk/src/filetypes.c
+++ libtifiles/trunk/src/filetypes.c
@@ -662,8 +662,7 @@ TIEXPORT2 int TICALL tifiles_file_is_flash(const char *filename)
  *
  * Return value: a boolean value.
  **/
-//TIEXPORT2 int TICALL tifiles_file_is_tib(const char *filename)
-int tifiles_file_is_tib(const char *filename)
+TIEXPORT2 int TICALL tifiles_file_is_tib(const char *filename)
 {
 	return tifiles_file_has_tib_header(filename);
 }
@@ -751,6 +750,11 @@ TIEXPORT2 int TICALL tifiles_file_test(const char *filename, FileClass type, Cal
 	{
 		if(target && !g_ascii_strcasecmp(e, FLASH_OS_FILE_EXT[target]))
 			return !0;
+		else if(target && tifiles_file_is_tib(filename)
+		{
+			// do more check (look for device type)
+			return !0;
+		}
 		else
 			return tifiles_file_is_group(filename);
 	}
