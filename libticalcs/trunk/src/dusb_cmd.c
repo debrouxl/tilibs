8f2cbf6471088e526efc6915aac4bbe2f5abd9f0
Romain Liévin <roms@lievin.net>
2007-02-04 14:01:04 +0000

    Removed debug print (already managed by the lower layer).
    git-svn-id: http://svn.tilp.info/repos/tilp@3146

--- libticalcs/trunk/src/dusb_cmd.c
+++ libticalcs/trunk/src/dusb_cmd.c
@@ -208,7 +208,6 @@ int cmd_s_mode_set(CalcHandle *h, ModeSet mode)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Ping / Set Mode");
 	ticalcs_info("   %04x %04x %04x %04x %04x", mode.arg1, mode.arg2, mode.arg3, mode.arg4, mode.arg5);
 
 	return 0;
@@ -227,7 +226,6 @@ int cmd_s_os_begin(CalcHandle *h, uint32_t size)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: OS Transfer Begin");
 	ticalcs_info("   size = %08x (%i)", size, size);
 
 	return 0;
@@ -251,7 +249,6 @@ int cmd_r_os_ack(CalcHandle *h, uint32_t *size)
 	*size = (pkt->data[0] << 24) | (pkt->data[1] << 16) | (pkt->data[2] << 8) | (pkt->data[3] << 0);
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: OS Transfer Ack");
 	ticalcs_info("   size = %08x (%i)", *size, *size);
 
 	return 0;
@@ -271,7 +268,6 @@ static int s_os(uint8_t type, CalcHandle *h, uint16_t addr, uint8_t page, uint8_
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: %s", type == VPKT_OS_HEADER ? "OS Header" : "OS Data");
 	ticalcs_info("   addr=%04x, page=%02x, flag=%02x, size=%04x", addr, page, flag, size);
 
 	return 0;
@@ -299,7 +295,6 @@ int cmd_s_os_header_89(CalcHandle *h, uint32_t size, uint8_t *data)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: OS Header");
 	ticalcs_info("   size = %08x (%i)", size, size);
 
 	return 0;
@@ -315,7 +310,6 @@ int cmd_s_os_data_89(CalcHandle *h, uint32_t size, uint8_t *data)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: OS Data");
 	ticalcs_info("   size = %08x (%i)", size, size);
 
 	return 0;
@@ -337,7 +331,6 @@ int cmd_r_eot_ack(CalcHandle *h)
 		return ERR_INVALID_PACKET;
 	
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: EOT Ack");
 
 	return 0;
 }
@@ -362,7 +355,6 @@ int cmd_s_param_request(CalcHandle *h, int npids, uint16_t *pids)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Parameter Request");
 	ticalcs_info("   npids=%i", npids);
 
 	return 0;
@@ -403,7 +395,6 @@ int cmd_r_param_data(CalcHandle *h, int nparams, CalcParam **params)
 	}
 	
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: Parameter Data");
 	ticalcs_info("   nparams=%i", nparams);
 
 	return 0;
@@ -437,7 +428,6 @@ int cmd_s_dirlist_request(CalcHandle *h, int naids, uint16_t *aids)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Directory Listing");
 	ticalcs_info("   naids=%i", naids);
 
 	return 0;
@@ -503,7 +493,6 @@ int cmd_r_var_header(CalcHandle *h, char *folder, char *name, CalcAttr **attr)
 	}
 	
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: Variable Header");
 	ticalcs_info("   folder=<%s>, name=<%s>", folder, name);
 
 	return 0;
@@ -558,7 +547,6 @@ int cmd_s_rts(CalcHandle *h, const char *folder, const char *name, uint32_t size
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Request to Send");
 	ticalcs_info("   folder=<%s>, name=<%s>, size=%i, nattrs=%i", folder, name, size, nattrs);
 
 	return 0;
@@ -622,7 +610,6 @@ int cmd_s_var_request(CalcHandle *h, const char *folder, const char *name,
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Request Variable");
 	ticalcs_info("   folder=<%s>, name=<%s>, naids=%i, nattrs=%i", folder, name, naids, nattrs);
 
 	return 0;
@@ -650,7 +637,6 @@ int cmd_r_var_content(CalcHandle *h, uint32_t *size, uint8_t **data)
 	memcpy(*data, pkt->data, pkt->size);
 	
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Variable Contents");
 	ticalcs_info("   size=%i", size);
 
 	return 0;
@@ -667,7 +653,6 @@ int cmd_s_var_content(CalcHandle *h, uint32_t size, uint8_t *data)
 	TRYF(dusb_send_data(h, pkt));
 	
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Variable Contents");
 	ticalcs_info("   size=%i", size);
 
 	return 0;
@@ -742,7 +727,6 @@ int cmd_s_var_delete(CalcHandle *h, const char *folder, const char *name, int na
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Delete Variable");
 	ticalcs_info("   folder=<%s>, name=<%s>, nattrs=%i", folder, name, nattrs);
 
 	return 0;
@@ -789,7 +773,6 @@ int cmd_s_execute(CalcHandle *h, const char *folder, const char *name,
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Remote Execute");
 	if(args)
 		ticalcs_info("   action=%i, folder=<%s>, name=<%s>, args=%s", action, folder, name, args);
 	else
@@ -814,7 +797,6 @@ int cmd_r_mode_ack(CalcHandle *h)
 		return ERR_INVALID_PACKET;
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: Mode Setting Ack");
 
 	return 0;
 }
@@ -835,7 +817,6 @@ int cmd_r_data_ack(CalcHandle *h)
 		return ERR_INVALID_PACKET;
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: Data Ack");
 
 	return 0;
 }
@@ -847,6 +828,7 @@ int cmd_r_delay_ack(CalcHandle *h)
 
 	pkt = vtl_pkt_new(0, 0);
 	TRYF(dusb_recv_data(h, pkt));
+	tifiles_hexdump(pkt->data, pkt->size);
 
 	if(pkt->type == VPKT_ERROR)
 		return ERR_CALC_ERROR2 + err_code(pkt);
@@ -856,7 +838,6 @@ int cmd_r_delay_ack(CalcHandle *h)
 	PAUSE(100);
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: Delay Ack");
 
 	return 0;
 }
@@ -870,7 +851,6 @@ int cmd_s_eot(CalcHandle *h)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: End of Transmission");
 
 	return 0;
 }
@@ -891,7 +871,6 @@ int cmd_r_eot(CalcHandle *h)
 		return ERR_INVALID_PACKET;
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  TI->PC: End of Transmission");
 
 	return 0;
 }
@@ -908,7 +887,6 @@ int cmd_s_error(CalcHandle *h, uint16_t code)
 	TRYF(dusb_send_data(h, pkt));
 
 	vtl_pkt_del(pkt);
-	//ticalcs_info("  PC->TI: Error");
 	ticalcs_info("   code = %04x", code);
 
 	return 0;
