f22e46535579c56e65239785d7e62c52995c5ad8
Romain Liévin <roms@lievin.net>
2007-04-11 21:43:32 +0000

    Status of battery (debug) is wrong.
    git-svn-id: http://svn.tilp.info/repos/tilp@3261

--- libticalcs/trunk/src/calc_73.c
+++ libticalcs/trunk/src/calc_73.c
@@ -890,7 +890,7 @@ static int		get_version	(CalcHandle* handle, CalcInfos* infos)
 	ticalcs_info(_("  OS: %s"), infos->os_version);
 	ticalcs_info(_("  BIOS: %s"), infos->boot_version);
 	ticalcs_info(_("  HW: %i"), infos->hw_version);
-	ticalcs_info(_("  Battery: %s"), (infos->battery & 1) ? "low" : "good");
+	ticalcs_info(_("  Battery: %s"), infos->battery ? "low" : "good");
 
 	return 0;
 }
