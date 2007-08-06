263930446c2b38419049409d839e328a0be82bcb
Romain Liévin <roms@lievin.net>
2007-08-06 22:24:39 +0000

    Make program more verbose when error occurs.
    git-svn-id: http://svn.tilp.info/repos/tilp@3689

--- libticables/trunk/src/win32/dha/ReadMe.txt
+++ libticables/trunk/src/win32/dha/ReadMe.txt
@@ -1,3 +1,4 @@
 You need a Windows DDK to build this driver.
+You need MinGW to build the executable.
 
 Those files comes from the MPlayer project (http://svn.mplayerhq.hu/mplayer/trunk/vidix/dhahelperwin/).
--- libticables/trunk/src/win32/dha/dhasetup.c
+++ libticables/trunk/src/win32/dha/dhasetup.c
@@ -5,42 +5,58 @@
 #include <windows.h>
 #include <stdio.h>
 
+static void print_last_error(char *s)
+{
+        LPTSTR lpMsgBuf;
+
+        FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, GetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) & lpMsgBuf, 0, NULL);
+
+		lpMsgBuf[strlen(lpMsgBuf)-2] = '\0';
+
+        printf("%s (%i -> %s)\n", s, GetLastError(), lpMsgBuf);
+}
+
 int main(int argc,char* argv[])
 {
   SC_HANDLE hSCManager = NULL;
   SC_HANDLE hService = NULL;
   char path[MAX_PATH];
+  int ret;
 
   printf("dhasetup (c) 2004 Sascha Sommer\n");
 
   if(argc==1)
   {
     printf("Usage:\n");
-    printf("dhasetup install -  Copies dhahelper.sys from the current directory to\n%s and configures it to start at boot.\n", path);
-    printf("dhasetup remove  -  Removes the dhahelper utility.\n");
+    printf("dhasetup install -  Copies DhaHelper.sys from the current directory to\n%s and configures it to start at boot.\n", path);
+    printf("dhasetup remove  -  Removes the DhaHelper utility.\n");
     return 0;
   }
 
-  
+  hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
 
   if(!strcmp(argv[1],"install"))
   {
-    printf("Installing dhahelper...\n");
+    printf("Installing DhaHelper...\n");
 
 	GetWindowsDirectory(path,MAX_PATH);
-	strcpy(path+strlen(path),"\\system32\\drivers\\dhahelper.sys");
+	strcpy(path+strlen(path),"\\system32\\drivers\\DhaHelper.sys");
 
-    if(!CopyFile("dhahelper.sys",path,FALSE))
+    if(!CopyFile("DhaHelper.sys",path,FALSE))
 	{
-      printf("Copying dhahelper.sys failed.\nEither dhahelper.sys is not in the current directory or you lack sufficient\nprivileges to write to %s.", path);
+      printf("Copying DhaHelper.sys failed.\nEither DhaHelper.sys is not in the current directory or you lack sufficient\nprivileges to write to %s.", path);
       return 1;
     }
 
     // Install the driver
-	hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     hService = CreateService(hSCManager,
-                             "DHAHELPER",
-                             "DHAHELPER",
+                             "DhaHelper",
+                             "DhaHelper",
                              SERVICE_ALL_ACCESS,
                              SERVICE_KERNEL_DRIVER,
                              SERVICE_SYSTEM_START,
@@ -53,19 +69,24 @@ int main(int argc,char* argv[])
                              NULL);
     if(!hService)
 	{
-      printf("Unable to register DHAHELPER Service (0x%x).\n",GetLastError());
+	  print_last_error("Unable to register DhaHelper Service");
     }
-
-	printf("Success!\n");
+	else
+	  printf("Success!\n");
   }
   else if(!strcmp(argv[1],"remove"))
   {
     SERVICE_STATUS ServiceStatus;
 
-    printf("Removing dhahelper... ");
-    hService = OpenService(hSCManager, "DHAHELPER", SERVICE_ALL_ACCESS);
-    ControlService(hService, SERVICE_CONTROL_STOP, &ServiceStatus);
-    DeleteService(hService);
+    printf("Removing DhaHelper...\n");
+    hService = OpenService(hSCManager, "DhaHelper", SERVICE_ALL_ACCESS);
+
+    ret = ControlService(hService, SERVICE_CONTROL_STOP, &ServiceStatus);
+	if(!ret) print_last_error("Error while stopping service");
+
+    ret = DeleteService(hService);
+	if(!ret) print_last_error("Error while deleting service");
+
     DeleteFile(path);
 	printf("Done!\n");
   }
