bc869d02c3eb80a8fa84e35ed1aa215cb7c9fb82
Romain Liévin <roms@lievin.net>
2007-08-06 20:12:22 +0000

    Slightly reworked code and made it more verbose.
    git-svn-id: http://svn.tilp.info/repos/tilp@3688

--- libticables/trunk/src/win32/dha/dhasetup.c
+++ libticables/trunk/src/win32/dha/dhasetup.c
@@ -5,27 +5,39 @@
 #include <windows.h>
 #include <stdio.h>
 
-int main(int argc,char* argv[]){
-  SC_HANDLE hSCManager;
-  SC_HANDLE hService;
+int main(int argc,char* argv[])
+{
+  SC_HANDLE hSCManager = NULL;
+  SC_HANDLE hService = NULL;
   char path[MAX_PATH];
+
   printf("dhasetup (c) 2004 Sascha Sommer\n");
-  GetWindowsDirectory(path,MAX_PATH);
-  strcpy(path+strlen(path),"\\system32\\drivers\\dhahelper.sys");
-  if(argc==1){
+
+  if(argc==1)
+  {
     printf("Usage:\n");
-    printf("dhasetup install  -  Copies dhahelper.sys from the current directory to\n%s and configures it to start at boot.\n", path);
+    printf("dhasetup install -  Copies dhahelper.sys from the current directory to\n%s and configures it to start at boot.\n", path);
     printf("dhasetup remove  -  Removes the dhahelper utility.\n");
     return 0;
   }
-  hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-  if(!strcmp(argv[1],"install")){
+
+  
+
+  if(!strcmp(argv[1],"install"))
+  {
     printf("Installing dhahelper...\n");
-    if(!CopyFile("dhahelper.sys",path,FALSE)){
+
+	GetWindowsDirectory(path,MAX_PATH);
+	strcpy(path+strlen(path),"\\system32\\drivers\\dhahelper.sys");
+
+    if(!CopyFile("dhahelper.sys",path,FALSE))
+	{
       printf("Copying dhahelper.sys failed.\nEither dhahelper.sys is not in the current directory or you lack sufficient\nprivileges to write to %s.", path);
       return 1;
     }
+
     // Install the driver
+	hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     hService = CreateService(hSCManager,
                              "DHAHELPER",
                              "DHAHELPER",
@@ -39,23 +51,33 @@ int main(int argc,char* argv[]){
                              NULL,
                              NULL,
                              NULL);
-    if(!hService){
+    if(!hService)
+	{
       printf("Unable to register DHAHELPER Service (0x%x).\n",GetLastError());
     }
+
+	printf("Success!\n");
   }
-  else if(!strcmp(argv[1],"remove")){
+  else if(!strcmp(argv[1],"remove"))
+  {
     SERVICE_STATUS ServiceStatus;
-    printf("Removing dhahelper...\n");
+
+    printf("Removing dhahelper... ");
     hService = OpenService(hSCManager, "DHAHELPER", SERVICE_ALL_ACCESS);
     ControlService(hService, SERVICE_CONTROL_STOP, &ServiceStatus);
     DeleteService(hService);
     DeleteFile(path);
+	printf("Done!\n");
   }
-  else {
+  else 
+  {
     printf("unknown parameter: %s\n",argv[1]);
   }
+
   CloseServiceHandle(hService);
   CloseServiceHandle(hSCManager);
+
   printf("Please reboot to let the changes take effect.\n");
+
   return 0;
 }
