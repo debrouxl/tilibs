d35c2d0dcc59ff610b625d1448a45cd9bf977433
Romain Liévin <roms@lievin.net>
2007-08-07 08:36:43 +0000

    Bumped revision.
    git-svn-id: http://svn.tilp.info/repos/tilp@3696

--- libticables/trunk/src/ticables.h
+++ libticables/trunk/src/ticables.h
@@ -45,7 +45,7 @@ extern "C" {
 /* Versioning */
 
 #ifdef __WIN32__
-# define libticables2_VERSION "1.0.6"
+# define libticables2_VERSION "1.0.7"
 #else
 # define libticables2_VERSION VERSION
 #endif
