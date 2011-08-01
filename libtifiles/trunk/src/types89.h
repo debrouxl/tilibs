2316d5aa0cf8ac6cec2081c706af41e1d4d39016
Lionel Debroux <lionel_debroux@yahoo.fr>
2011-08-01 09:24:05 +0000

    all: upgrade COPYING files and FSF addresses embedded in files, so as to make rpmlint happier. The occurrences in */aclocal.m4 are out of my control, autoreconf restores them.
    git-svn-id: http://svn.tilp.info/repos/tilp@4392

--- libtifiles/trunk/src/types89.h
+++ libtifiles/trunk/src/types89.h
@@ -3,6 +3,7 @@
 
 /*  libtifiles - file format library, a part of the TiLP project
  *  Copyright (C) 1999-2005  Romain Lievin
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -14,8 +15,8 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef __TIFILES_DEFS89__
