0252e9a8c7bee7049cbe8bebd9a715b9771cd376
Adrien Bertrand <bertrand.adrien@gmail.com>
2019-01-02 17:05:55 +0100

    [cmake] use GNUInstallDirs variables for more portability

--- .cmake/configure_and_install_pc_file.cmake
+++ .cmake/configure_and_install_pc_file.cmake
@@ -1,13 +1,12 @@
 macro(configure_and_install_pc_file name version)
     set(prefix      "${CMAKE_INSTALL_PREFIX}")
     set(exec_prefix "\${prefix}")
-    set(libdir      "${CMAKE_INSTALL_PREFIX}/lib")
-    set(includedir  "${CMAKE_INSTALL_PREFIX}/include")
+    set(libdir      "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    set(includedir  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
     set(VERSION     "${version}")
 
     configure_file(${name}.pc.in ${PROJECT_BINARY_DIR}/${name}.pc @ONLY)
     set(ENV{PKG_CONFIG_PATH} "${PROJECT_BINARY_DIR}:$ENV{PKG_CONFIG_PATH}")
 
-    install(FILES       ${CMAKE_CURRENT_BINARY_DIR}/${name}.pc
-            DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${name}.pc DESTINATION "${libdir}/pkgconfig")
 endmacro()
