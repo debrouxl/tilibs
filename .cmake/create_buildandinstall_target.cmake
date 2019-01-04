aa1b485bbfc55e58e8f7e9778634b3ea0d6adafd
Adrien Bertrand <bertrand.adrien@gmail.com>
2019-01-04 01:55:51 +0100

    [cmake] simplify everything, use specific target commands, better scoping

--- .cmake/create_buildandinstall_target.cmake
+++ .cmake/create_buildandinstall_target.cmake
@@ -1,8 +1,8 @@
-macro(create_buildandinstall_target name)
+function(create_buildandinstall_target name)
     file(RELATIVE_PATH TAR_INS_NAME ${CMAKE_SOURCE_DIR} "${PROJECT_SOURCE_DIR}")
     add_custom_target(buildandinstall_${name}
         DEPENDS ${ARGN}
         COMMAND ${CMAKE_COMMAND} -P "${TAR_INS_NAME}/cmake_install.cmake" --config $<CONFIGURATION>
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
     )
-endmacro()
+endfunction()
