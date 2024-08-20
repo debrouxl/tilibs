4b1b4ea7d0a79e7845aa1e11f9cf37c1dea38061
Adrien Bertrand <bertrand.adrien@gmail.com>
2024-08-20 16:19:59 +0200

    cmake/ci: massive cleanup & fixes, use more vcpkg/presets capabilities

--- .cmake/configure_and_install_pc_file.cmake
+++ .cmake/configure_and_install_pc_file.cmake
@@ -1,8 +1,8 @@
 function(configure_and_install_pc_file name version)
     set(prefix      "${CMAKE_INSTALL_PREFIX}")
     set(exec_prefix "\${prefix}")
-    set(libdir      "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
-    set(includedir  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
+    set(libdir      "\${prefix}/${CMAKE_INSTALL_LIBDIR}")
+    set(includedir  "\${prefix}/${CMAKE_INSTALL_INCLUDEDIR}")
     set(VERSION     "${version}")
 
     configure_file(${name}.pc.in ${PROJECT_BINARY_DIR}/${name}.pc @ONLY)
